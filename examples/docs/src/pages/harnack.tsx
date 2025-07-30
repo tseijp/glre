e5c2c4cc7d94a227431ff518ec055ebf749f2a51
tseijp <_@tsei.jp>
2025-07-30 17:16:16 +0900

    fix: harnack demo

--- examples/docs/src/pages/harnack.tsx
+++ examples/docs/src/pages/harnack.tsx
@@ -6,6 +6,7 @@ import {
         constant,
         float,
         iResolution,
+        iTime,
         int,
         mat3,
         position,
@@ -18,35 +19,26 @@ import {
         vec3,
         vec4,
 } from 'glre/src/react'
+import { useControls } from 'leva'
 import { useDrag } from 'rege/react'
 
+// constant
 const iterations = constant(int(200))
 const epsilon = constant(float(0.01))
-const unit_shift = constant(float(3))
-const sphereRadius = constant(float(1))
-const thickness = constant(float(0.01))
-const ambient = constant(float(0.01))
-const metallic = constant(float(0.01))
-const roughness = constant(float(0.01))
-const baseColor = constant(vec3(0.7, 0.6, 0.7))
-const sphereCenter = constant(vec3(0, 0, 0))
-const scale = uniform(float(4))
-const lightPosition = uniform(vec3(10, 10, 1))
-const cameraRotation = uniform(vec2(0))
-const cameraPosition = uniform(vec3(0, 0, 3))
+const lightScale = constant(float(0.02))
+const lightPosition = vec3(iTime.sin(), iTime.sin().mul(2), iTime.cos())
 
-const gyroid = Fn(([pos]) => {
-        const p = scale.mul(pos).toVar('p')
-        const c = p.cos().toVar('c')
-        const s = p.sin().toVar('s')
-        const t = s.x.mul(c.y).add(s.y.mul(c.z)).add(s.z.mul(c.x))
-        const n = vec3(
-                c.x.mul(c.y).sub(s.z.mul(s.x)),
-                c.y.mul(c.z).sub(s.x.mul(s.y)),
-                c.z.mul(c.x).sub(s.y.mul(s.z))
-        ).mul(scale)
-        return Ray({ hit: bool(true), t, n })
-})
+// uniforms
+const thickness = uniform(float(), 'thickness')
+const ambient = uniform(float(), 'ambient')
+const metallic = uniform(float(), 'metallic')
+const roughness = uniform(float(), 'roughness')
+const scale = uniform(float(), 'scale')
+const radius = uniform(float(), 'radius')
+const unitShift = uniform(float(), 'unitShift')
+const baseColor = uniform(vec3(), 'baseColor')
+const cameraRotation = uniform(vec2(0))
+const cameraPosition = uniform(vec3(), 'cameraPosition')
 
 const closeToLevelset = Fn(([f, levelset, tol, gradNorm]) => {
         const eps = tol.mul(gradNorm).toVar('eps')
@@ -65,60 +57,75 @@ const Ray = struct({
         n: vec3(),
 })
 
+const gyroid = Fn(([pos]) => {
+        const p = scale.mul(pos).toVar('p')
+        const c = p.cos().toVar('c')
+        const s = p.sin().toVar('s')
+        const t = s.x.mul(c.y).add(s.y.mul(c.z)).add(s.z.mul(c.x))
+        const n = vec3(
+                c.x.mul(c.y).sub(s.z.mul(s.x)),
+                c.y.mul(c.z).sub(s.x.mul(s.y)),
+                c.z.mul(c.x).sub(s.y.mul(s.z))
+        ).mul(scale)
+        return Ray({ hit: bool(true), t, n })
+})
+
 const intersectSphere = Fn(([ro, rd, center, radius]) => {
         const a = ro.sub(center).toVar('a')
         const b = a.dot(rd).toVar('b')
         const c = a.dot(a).sub(radius.mul(radius)).toVar('c')
         const d = b.mul(b).sub(c).toVar('d')
+        const ray = Ray({ hit: bool(false), t: float(-1), n: vec3(0) })
         If(d.lessThan(0), () => {
-                return Ray({ hit: bool(false), t: float(-1), n: vec3(0) })
+                return ray
         })
-        const t = b.negate().sub(d.sqrt()).toVar('t')
-        If(t.lessThan(0), () => {
-                return Ray({ hit: bool(false), t: float(-1), n: vec3(0) })
+        ray.t = b.negate().sub(d.sqrt()).toVar('t')
+        If(ray.t.lessThan(0), () => {
+                return ray
         })
-        const n = ro.add(rd.mul(t)).sub(center).div(radius).toVar('normal')
-        return Ray({ hit: bool(true), t, n })
+        ray.n = rd.mul(ray.t).add(ro).sub(center).div(radius).toVar('normal')
+        ray.hit = bool(true)
+        return ray
 })
 
 const harnack = Fn(([ro, rd]) => {
-        const t = float(0).toVar('t')
         const levelset = float(0).toVar('levelset')
-        const sphereHit = intersectSphere(ro, rd, sphereCenter, sphereRadius).toVar('sphereHit')
-        If(sphereHit.hit.not(), () => {
+        const sphere = intersectSphere(ro, rd, vec3(0), radius).toVar('sphere')
+        If(sphere.hit.not(), () => {
                 return Ray({ hit: bool(false), t: float(0), n: vec3(0) })
         })
-        If(sphereHit.t.greaterThan(0), () => {
-                t.assign(sphereHit.t)
-        })
-        const pos = ro.add(t.mul(rd)).toVar('pos')
-        const ray = gyroid(pos).toVar('ray')
-        const eps = epsilon.mul(ray.n.length()).toVar('eps')
+        const pos = sphere.t.mul(rd).add(ro).toVar('pos')
+        const gyr = gyroid(pos).toVar('gyr')
         If(
                 levelset
                         .sub(thickness)
-                        .sub(ray.t)
-                        .max(ray.t.sub(levelset.add(thickness)))
-                        .lessThan(eps),
+                        .sub(gyr.t)
+                        .max(gyr.t.sub(levelset.add(thickness)))
+                        .lessThan(epsilon.mul(gyr.n.length())),
                 () => {
-                        return ray
+                        return gyr
                 }
         )
+        const t = sphere.t.toVar('t')
         const overstep = float(0).toVar('overstep')
         Loop(iterations, () => {
-                pos.assign(ro.add(t.mul(rd)).add(overstep.mul(rd)))
-                ray.assign(gyroid(pos))
-                const R = sphereRadius.add(0.5).sub(pos.length()).toVar('R')
-                const shift = sqrt(2).mul(R).exp().mul(unit_shift).toVar('shift')
-                const r = getMaxStep4D(ray.t, R, levelset, shift).toVar('r')
-                If(r.greaterThanEqual(overstep).and(closeToLevelset(ray.t, levelset, epsilon, ray.n.length())), () => {
-                        return ray
+                pos.assign(t.mul(rd).add(ro).add(overstep.mul(rd)))
+                gyr.assign(gyroid(pos))
+                const R = radius.add(0.5).sub(pos.length()).toVar('R')
+                const shift = sqrt(2).mul(R).exp().mul(unitShift).toVar('shift')
+                const r = getMaxStep4D(gyr.t, R, levelset, shift).toVar('r')
+
+                If(r.greaterThan(overstep), () => {
+                        If(closeToLevelset(gyr.t, levelset, epsilon, gyr.n.length()), () => {
+                                gyr.t = t
+                                return gyr
+                        })
+                        overstep.assign(r.mul(0.75))
+                        t.assign(t.add(overstep.add(r)))
                 })
-                const stepSize = r.greaterThanEqual(overstep).toFloat().mul(overstep.add(r)).toVar('stepSize')
-                overstep.assign(r.greaterThanEqual(overstep).toFloat().mul(r.mul(0.75)))
-                t.assign(t.add(stepSize))
         })
-        return ray
+        gyr.t.assign(t)
+        return gyr
 })
 
 const shade = Fn(([hitPos = vec3(), rd, nor]) => {
@@ -157,16 +164,11 @@ const shade = Fn(([hitPos = vec3(), rd, nor]) => {
 const fragment = Fn(([position = vec4()]) => {
         const cr = cameraRotation.cos().toVar('cr')
         const sr = cameraRotation.sin().toVar('sr')
+        // prettier-ignore
         const mat = mat3(
-                cr.y,
-                0,
-                sr.y.negate(),
-                sr.x.mul(sr.y),
-                cr.x,
-                cr.y.mul(sr.x),
-                cr.x.mul(sr.y),
-                sr.x.negate(),
-                cr.x.mul(cr.y)
+                cr.y,           0,             sr.y.negate(),
+                sr.x.mul(sr.y), cr.x,          cr.y.mul(sr.x),
+                cr.x.mul(sr.y), sr.x.negate(), cr.x.mul(cr.y)
         ).toVar('mat')
         const ro = mat.mul(cameraPosition).toVar('camPos')
         const fov = radians(50).mul(0.5).tan().toVec2().toVar('fov')
@@ -174,13 +176,18 @@ const fragment = Fn(([position = vec4()]) => {
         const coord = position.xy.div(iResolution.xy).mul(2).sub(1).toVar('coord')
         const vDir = vec3(coord.mul(fov), -1).normalize().toVar('vDir')
         const rd = mat.mul(vDir).toVar('rd')
-        const ray = harnack(ro, rd).toVar('ray')
-        If(ray.hit, () => {
-                const nor = ray.n.normalize().toVar('nor')
-                const hitPos = ro.add(ray.t.mul(rd)).toVar('hitPos')
-                const shadedColor = shade(hitPos, rd, nor).toVar('shadedColor')
-                return vec4(shadedColor.pow(vec3(1.0 / 2.2)), 1)
+        const ray = harnack(ro, rd)
+        const light = intersectSphere(ro, rd, lightPosition, lightScale)
+        If(light.hit.and(ray.hit.not().or(light.t.lessThan(ray.t))), () => {
+                return vec4(1)
         })
+        If(ray.hit, () => {
+                const nor = ray.n.normalize()
+                const hitPos = ray.t.mul(rd).add(ro)
+                const shadedColor = shade(hitPos, rd, nor)
+                return vec4(shadedColor.pow(vec3(1 / 2.2)), 1)
+        })
+
         return vec4(0)
 })
 
@@ -190,8 +197,8 @@ export default function App() {
         })
         const update = () => {
                 const [x, y] = drag.offset
-                const rotX = y / 200
-                const rotY = x / 200
+                const rotX = y / 100
+                const rotY = x / 100
                 cameraRotation.value = [-rotX, -rotY]
         }
         const gl = useGL({
@@ -201,6 +208,20 @@ export default function App() {
                         update()
                 },
         })
+        gl.uniform(
+                useControls({
+                        thickness: { value: 0, min: 0, max: 2 },
+                        ambient: { value: 0.1, min: 0, max: 1 },
+                        metallic: { value: 1, min: 0, max: 1 },
+                        roughness: { value: 0.7, min: 0, max: 1 },
+                        scale: { value: 4, min: 0, max: 10 },
+                        radius: { value: 1, min: 0, max: 2 },
+                        unitShift: { value: 3, min: 0, max: 10 },
+                        baseColor: [0.8, 0.7, 0.8],
+                        cameraPosition: [0, 0, 3],
+                })
+        )
+
         return (
                 <div ref={drag.ref} style={{ touchAction: 'none' }}>
                         <canvas ref={gl.ref} />
