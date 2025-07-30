09c298253275c964b1f913258d21441ebe3528d5
tseijp <_@tsei.jp>
2025-07-30 15:24:32 +0900

    chore: format of code

--- examples/docs/src/pages/harnack.tsx
+++ examples/docs/src/pages/harnack.tsx
@@ -9,6 +9,7 @@ import {
         int,
         mat3,
         position,
+        radians,
         sqrt,
         struct,
         uniform,
@@ -19,16 +20,16 @@ import {
 } from 'glre/src/react'
 import { useDrag } from 'rege/react'
 
+const iterations = constant(int(200))
 const epsilon = constant(float(0.01))
-const max_iterations = constant(int(250))
 const unit_shift = constant(float(3))
-const thickness = constant(float(0.1))
-const baseColor = constant(vec3(0.7, 0.6, 0.7))
-const sphereCenter = constant(vec3(0, 0, 0))
 const sphereRadius = constant(float(1))
+const thickness = constant(float(0.01))
+const ambient = constant(float(0.01))
 const metallic = constant(float(0.01))
 const roughness = constant(float(0.01))
-const ambient = constant(float(0.01))
+const baseColor = constant(vec3(0.7, 0.6, 0.7))
+const sphereCenter = constant(vec3(0, 0, 0))
 const scale = uniform(float(4))
 const lightPosition = uniform(vec3(10, 10, 1))
 const cameraRotation = uniform(vec2(0))
@@ -91,32 +92,27 @@ const harnack = Fn(([ro, rd]) => {
                 t.assign(sphereHit.t)
         })
         const pos = ro.add(t.mul(rd)).toVar('pos')
-        const gyr = gyroid(pos).toVar('gyr')
-        const val = gyr.t.toVar('val')
-        const n = gyr.n.toVar('n')
-        const eps = epsilon.mul(n.length()).toVar('eps')
+        const ray = gyroid(pos).toVar('ray')
+        const eps = epsilon.mul(ray.n.length()).toVar('eps')
         If(
                 levelset
                         .sub(thickness)
-                        .sub(val)
-                        .max(val.sub(levelset.add(thickness)))
+                        .sub(ray.t)
+                        .max(ray.t.sub(levelset.add(thickness)))
                         .lessThan(eps),
                 () => {
-                        return Ray({ hit: bool(true), t, n })
+                        return ray
                 }
         )
         const overstep = float(0).toVar('overstep')
-        const ray = Ray({ hit: bool(false), t: float(0), n: vec3(0) }).toVar('ray')
-        Loop(max_iterations, () => {
+        Loop(iterations, () => {
                 pos.assign(ro.add(t.mul(rd)).add(overstep.mul(rd)))
-                const loopGyr = gyroid(pos).toVar('loopGyr')
-                val.assign(loopGyr.t)
-                n.assign(loopGyr.n)
+                ray.assign(gyroid(pos))
                 const R = sphereRadius.add(0.5).sub(pos.length()).toVar('R')
                 const shift = sqrt(2).mul(R).exp().mul(unit_shift).toVar('shift')
-                const r = getMaxStep4D(val, R, levelset, shift).toVar('r')
-                If(r.greaterThanEqual(overstep).and(closeToLevelset(val, levelset, epsilon, n.length())), () => {
-                        ray.assign(Ray({ hit: bool(true), t, n }))
+                const r = getMaxStep4D(ray.t, R, levelset, shift).toVar('r')
+                If(r.greaterThanEqual(overstep).and(closeToLevelset(ray.t, levelset, epsilon, ray.n.length())), () => {
+                        return ray
                 })
                 const stepSize = r.greaterThanEqual(overstep).toFloat().mul(overstep.add(r)).toVar('stepSize')
                 overstep.assign(r.greaterThanEqual(overstep).toFloat().mul(r.mul(0.75)))
@@ -140,22 +136,20 @@ const shade = Fn(([hitPos = vec3(), rd, nor]) => {
         const dotNV = viewDir.dot(outwardNormal).max(0).toVar('dotNV')
         const dotNH = halfDir.dot(outwardNormal).max(0).toVar('dotNH')
         const dotLH = lightDir.dot(halfDir).max(0).toVar('dotLH')
-        const alpha = roughness.mul(roughness).toVar('alpha')
-        const alphaSqr = alpha.mul(alpha).toVar('alphaSqr')
+        const alphaSqr = roughness.pow(4).toVar('alphaSqr')
         const denom = dotNH.mul(dotNH).mul(alphaSqr.sub(1)).add(1).toVar('denom')
         const D = alphaSqr.div(constant(3.14159).mul(denom).mul(denom)).toVar('D')
         const dotLH5 = dotLH.oneMinus().pow(5).toVar('dotLH5')
-        const F0 = constant(0.2).toVar('F0')
-        const F = F0.add(F0.oneMinus().mul(dotLH5)).toVar('F')
-        const k = alpha.div(2).toVar('k')
-        const G1L = float(1).div(dotNL.mul(k.oneMinus()).add(k)).toVar('G1L')
-        const G1V = float(1).div(dotNV.mul(k.oneMinus()).add(k)).toVar('G1V')
-        const G = G1L.mul(G1V).toVar('G')
+        const F = constant(0.2).add(constant(0.8).mul(dotLH5)).toVar('F')
+        const k = roughness.mul(roughness).div(2).toVar('k')
+        const G = float(1)
+                .div(dotNL.mul(k.oneMinus()).add(k))
+                .mul(float(1).div(dotNV.mul(k.oneMinus()).add(k)))
+                .toVar('G')
         const specular = D.mul(F).mul(G).toVar('specular')
         const diffuse = baseColor.pow(vec3(2.2)).mul(dotNL).toVar('diffuse')
         const fresnel = viewDir.dot(outwardNormal).oneMinus().max(0).toVar('fresnel')
-        const fresnelPow = metallic.mul(1.5).add(3.5).toVar('fresnelPow')
-        const fresnelFactor = fresnel.pow(fresnelPow).toVar('fresnelFactor')
+        const fresnelFactor = fresnel.pow(metallic.mul(1.5).add(3.5)).toVar('fresnelFactor')
         const reflection = baseColor.mul(fresnelFactor).toVar('reflection')
         return diffuse.mul(fresnelFactor.oneMinus()).add(reflection).add(vec3(specular)).add(vec3(ambient)).min(vec3(1))
 })
@@ -163,7 +157,7 @@ const shade = Fn(([hitPos = vec3(), rd, nor]) => {
 const fragment = Fn(([position = vec4()]) => {
         const cr = cameraRotation.cos().toVar('cr')
         const sr = cameraRotation.sin().toVar('sr')
-        const v2wRotMat = mat3(
+        const mat = mat3(
                 cr.y,
                 0,
                 sr.y.negate(),
@@ -173,14 +167,13 @@ const fragment = Fn(([position = vec4()]) => {
                 cr.x.mul(sr.y),
                 sr.x.negate(),
                 cr.x.mul(cr.y)
-        ).toVar('v2wRotMat')
-        const camPos = v2wRotMat.mul(cameraPosition).toVar('camPos')
-        const tanHalfFov = constant(50).radians().mul(0.5).tan().toVec2().toVar('tanHalfFov')
-        tanHalfFov.x.assign(tanHalfFov.x.mul(iResolution.x.div(iResolution.y)))
-        const cCoord = position.xy.div(iResolution.xy).mul(2).sub(1).toVar('cCoord')
-        const vDir = vec3(cCoord.mul(tanHalfFov), -1).normalize().toVar('vDir')
-        const rd = v2wRotMat.mul(vDir).toVar('rd')
-        const ro = camPos.toVar('ro')
+        ).toVar('mat')
+        const ro = mat.mul(cameraPosition).toVar('camPos')
+        const fov = radians(50).mul(0.5).tan().toVec2().toVar('fov')
+        fov.x.assign(fov.x.mul(iResolution.x.div(iResolution.y)))
+        const coord = position.xy.div(iResolution.xy).mul(2).sub(1).toVar('coord')
+        const vDir = vec3(coord.mul(fov), -1).normalize().toVar('vDir')
+        const rd = mat.mul(vDir).toVar('rd')
         const ray = harnack(ro, rd).toVar('ray')
         If(ray.hit, () => {
                 const nor = ray.n.normalize().toVar('nor')
