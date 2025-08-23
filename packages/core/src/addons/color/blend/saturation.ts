bc6080121d975a0ff868552988a6c3caa9401061
tseijp <_@tsei.jp>
2025-08-23 12:45:18 +0000

    feat: implement blendSaturation HSV chroma channel filter (#84)

---
+++ packages/core/src/addons/color/blend/saturation.ts
@@ -0,0 +1,28 @@
+import { Fn, Vec3, Float, vec3 } from '../../../node'
+import { rgb2hsv } from '../space/rgb2hsv'
+import { hsv2rgb } from '../space/hsv2rgb'
+
+export const blendSaturation = Fn(([base, blend]: [Vec3, Vec3]): Vec3 => {
+        const baseHSV = rgb2hsv(base)
+        const blendHSV = rgb2hsv(blend)
+        return hsv2rgb(vec3(baseHSV.x, blendHSV.y, baseHSV.z))
+}).setLayout({
+        name: 'blendSaturation',
+        type: 'vec3',
+        inputs: [
+                { name: 'base', type: 'vec3' },
+                { name: 'blend', type: 'vec3' }
+        ]
+})
+
+export const blendSaturationOpacity = Fn(([base, blend, opacity]: [Vec3, Vec3, Float]): Vec3 => {
+        return blendSaturation(base, blend).mul(opacity).add(base.mul(opacity.oneMinus()))
+}).setLayout({
+        name: 'blendSaturationOpacity',
+        type: 'vec3',
+        inputs: [
+                { name: 'base', type: 'vec3' },
+                { name: 'blend', type: 'vec3' },
+                { name: 'opacity', type: 'float' }
+        ]
+})
