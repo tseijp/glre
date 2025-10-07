37957477ebc06066b8421e596a5d96ab8a276110
tseijp <_@tsei.jp>
2025-10-07 14:55:41 +0900

    :tada:party initialize (#88)

---
+++ examples/party/vite.config.ts
@@ -0,0 +1,7 @@
+import { cloudflare } from '@cloudflare/vite-plugin'
+import { defineConfig } from 'vite'
+import ssrPlugin from 'vite-ssr-components/plugin'
+
+export default defineConfig({
+        plugins: [cloudflare(), ssrPlugin()],
+})
