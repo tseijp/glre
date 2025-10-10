3072eec81e9664a3f87f2a6986434d54fc962744
tseijp <_@tsei.jp>
2025-10-10 21:48:35 +0900

    chore: wip

--- examples/party/src/canvas.tsx
+++ examples/party/src/canvas.tsx
@@ -1,7 +1,10 @@
 import { useGL } from 'glre/src/react'
+import { float, uv, vec3, vec4 } from 'glre/src/node'
 
 export const Canvas = () => {
-        const gl = useGL()
+        const gl = useGL({
+                fragment: vec4(vec3(float(0.3).mix(0.6, uv.y)), 1),
+        })
         return <canvas ref={gl.ref} className="w-full h-full" />
 }
 
