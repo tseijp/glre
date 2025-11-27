fdfc0f39511681f53062ebea1bf4dc31c6f49bd9
sei <40712342+tseijp@users.noreply.github.com>
2025-11-27 14:05:32 +0900

    Optimize swizzle inference mappings

--- packages/core/src/node/types.ts
+++ packages/core/src/node/types.ts
@@ -100,13 +100,13 @@ export interface NodeContext {
 /**
  * infer
  */
-// Optimized string length using direct pattern matching
+// Optimized swizzle length using fixed-depth checks instead of recursion
 // prettier-ignore
-type _StringLength<A extends string> =
-        A extends `${infer _}${infer A}` ? A extends '' ? 1 :
-        A extends `${infer _}${infer B}` ? B extends '' ? 2 :
-        B extends `${infer _}${infer C}` ? C extends '' ? 3 :
-        4 : never : never : never
+type _SwizzleLength<S extends string> =
+        S extends `${string}${string}${string}${string}${string}` ? never :
+        S extends `${string}${string}${string}${string}` ? 4 :
+        S extends `${string}${string}${string}` ? 3 :
+        S extends `${string}${string}` ? 2 : 1
 
 // Unified logic with infer.ts inferOperator function
 // prettier-ignore
@@ -138,11 +138,42 @@ type IsInRules<L extends C, R extends C> = [L, R] extends OperatorTypeRules ? 1 
 type ValidateOperator<L extends C, R extends C> = L extends R ? 1 : IsInRules<L, R>
 
 // prettier-ignore
-type InferSwizzleType<S extends string> =
-        _StringLength<S> extends 4 ? 'vec4' :
-        _StringLength<S> extends 3 ? 'vec3' :
-        _StringLength<S> extends 2 ? 'vec2' :
-        'float'
+type _SwizzleBase<T extends C> = T extends keyof SwizzleBaseMap ? SwizzleBaseMap[T] : 'float'
+
+type SwizzleBaseMap = {
+        float: 'float'
+        vec2: 'float'
+        vec3: 'float'
+        vec4: 'float'
+        int: 'int'
+        ivec2: 'int'
+        ivec3: 'int'
+        ivec4: 'int'
+        uint: 'uint'
+        uvec2: 'uint'
+        uvec3: 'uint'
+        uvec4: 'uint'
+        bool: 'bool'
+        bvec2: 'bool'
+        bvec3: 'bool'
+        bvec4: 'bool'
+}
+
+type SwizzleResultMap = {
+        float: { 1: 'float'; 2: 'vec2'; 3: 'vec3'; 4: 'vec4' }
+        int: { 1: 'int'; 2: 'ivec2'; 3: 'ivec3'; 4: 'ivec4' }
+        uint: { 1: 'uint'; 2: 'uvec2'; 3: 'uvec3'; 4: 'uvec4' }
+        bool: { 1: 'bool'; 2: 'bvec2'; 3: 'bvec3'; 4: 'bvec4' }
+}
+
+// prettier-ignore
+type _SwizzleResult<Base extends C, L extends 1 | 2 | 3 | 4> = SwizzleResultMap[_SwizzleBase<Base>][L]
+
+// prettier-ignore
+type InferSwizzleType<T extends C, S extends string> =
+        _SwizzleLength<S> extends infer L extends 1 | 2 | 3 | 4
+                ? _SwizzleResult<_SwizzleBase<T>, L>
+                : 'float'
 
 /**
  * Swizzles
@@ -218,7 +249,7 @@ export interface ConstantsToType {
 type XImpl<T extends C> = _X<T> & {
         [K in string as K extends Methods ? never : K]: any
 } & {
-        [K in Swizzles]: X<InferSwizzleType<K>>
+        [K in Swizzles]: X<InferSwizzleType<T, K>>
 }
 
 type C = Constants
