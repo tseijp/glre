fa60f61ab7c002a725abc770b5c917bf58ca7403
tseijp <_@tsei.jp>
2025-12-26 17:23:52 +0900

    chore: format of code

--- examples/docs/src/pages/environ.tsx
+++ examples/docs/src/pages/environ.tsx
@@ -4,7 +4,46 @@ import { useGL } from 'glre/src/react'
 import { attribute, Fn, mat4, Scope, texture, uniform, uv, varying, vec2, vec3, Vec3, vec4 } from 'glre/src/node'
 import { mat4 as m } from 'gl-matrix'
 
-export default function EnvironmentMap() {
+const createSphere = (radius = 0.5, widthSegments = 16, heightSegments = 12, phiStart = 0, phiLength = Math.PI * 2, thetaStart = 0, thetaLength = Math.PI) => {
+        const vertices = []
+        const normals = []
+        for (let y = 0; y <= heightSegments; y++) {
+                const v = y / heightSegments
+                const theta = thetaStart + v * thetaLength
+                for (let x = 0; x <= widthSegments; x++) {
+                        const u = x / widthSegments
+                        const phi = phiStart + u * phiLength
+                        const st = Math.sin(theta)
+                        const ct = Math.cos(theta)
+                        const sp = Math.sin(phi)
+                        const cp = Math.cos(phi)
+                        const px = radius * st * cp
+                        const py = radius * ct
+                        const pz = radius * st * sp
+                        vertices.push(px, py, pz)
+                        normals.push(st * cp, ct, st * sp)
+                }
+        }
+        const indices = []
+        for (let y = 0; y < heightSegments; y++) {
+                for (let x = 0; x < widthSegments; x++) {
+                        const a = (y + 1) * (widthSegments + 1) + x
+                        const b = (y + 1) * (widthSegments + 1) + x + 1
+                        const c = y * (widthSegments + 1) + x
+                        const d = y * (widthSegments + 1) + x + 1
+                        indices.push(a, c, b, b, c, d)
+                }
+        }
+        const ret = { positions: [], normals: [] }
+        for (let i = 0; i < indices.length; i++) {
+                const idx = indices[i] * 3
+                ret.positions.push(vertices[idx], vertices[idx + 1], vertices[idx + 2])
+                ret.normals.push(normals[idx], normals[idx + 1], normals[idx + 2])
+        }
+        return ret
+}
+
+export default function EnvironApp() {
         let yaw = 0
         let pitch = 0
         let mouseDown = false
@@ -15,8 +54,9 @@ export default function EnvironmentMap() {
         const cam = uniform(vec3())
         const mat = uniform(mat4())
         const inv = uniform(mat4())
-        const cube = attribute(vec3([-0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, 0.5, -0.5, 0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, -0.5, -0.5, 0.5, -0.5, -0.5, -0.5, -0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, -0.5, -0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, -0.5, -0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5]), 'cube')
-        const norm = attribute(vec3([-1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1]), 'norm')
+        const geo = createSphere(1, 128, 128)
+        const norm = attribute(vec3(geo.normals), 'norm')
+        const sphere = attribute(vec3(geo.positions), 'sphere')
         const update = () => {
                 const cos = Math.cos(pitch)
                 const pos = [2 * Math.sin(yaw) * cos, 2 * Math.sin(pitch), 2 * Math.cos(yaw) * cos]
@@ -27,24 +67,26 @@ export default function EnvironmentMap() {
                 mat.value = VP
                 inv.value = m.invert(tmp2, VP)
         }
-        const _uv = Fn(([d]: [Vec3]) => {
-                d = d.normalize().toVar()
-                return vec2(d.x.atan2(d.z), d.y.asin()).div(vec2(6.28, 3.14)).fma(vec2(1, -1), 0.5)
+        const dir2uv = Fn(([d]: [Vec3]) => {
+                return vec2(d.x.atan2(d.z), d.y.asin()).div(vec2(6.28, -3.14)).add(0.5)
         })
         const gl = useGL(
                 {
                         isDepth: true,
                         isWebGL: true,
-                        fragment: texture(env, _uv(inv.mul(vec4(uv.fma(2, -1), 1, 1)).xyz)),
+                        fragment: Scope(() => {
+                                const d = inv.mul(vec4(uv.fma(2, -1), 1, 1)).xyz.normalize()
+                                return texture(env, dir2uv(d))
+                        }),
                 },
                 {
-                        triangleCount: 12,
-                        vertex: mat.mul(vec4(cube, 1)),
+                        triangleCount: geo.positions.length / 9,
+                        vertex: mat.mul(vec4(sphere, 1)),
                         fragment: Scope(() => {
-                                const p = varying(cube)
+                                const p = varying(sphere)
                                 const n = varying(norm)
-                                const d = p.sub(cam).normalize().reflect(n)
-                                return texture(env, _uv(d))
+                                const d = p.sub(cam).normalize().reflect(n).normalize()
+                                return texture(env, dir2uv(d))
                         }),
                         render() {
                                 if (mouseDown) return
