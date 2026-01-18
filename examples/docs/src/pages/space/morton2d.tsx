3c995fe57213dab644e6f48d7c732411c7575438
tseijp <_@tsei.jp>
2026-01-18 16:58:03 +0900

    chore: format of code (#137)

--- examples/docs/src/pages/space/morton2d.tsx
+++ examples/docs/src/pages/space/morton2d.tsx
@@ -1,37 +1,91 @@
+/**
+ * ref: https://www.shadertoy.com/view/dtsczl
+// Switch between methods of converting float -> uint
+#define USE_NEW_MAPPING 0
+uint Part1By1(uint x) {
+        x &= 0x0000ffffu;                  // x = ---- ---- ---- ---- fedc ba98 7654 3210
+        x = (x ^ (x <<  8)) & 0x00ff00ffu; // x = ---- ---- fedc ba98 ---- ---- 7654 3210
+        x = (x ^ (x <<  4)) & 0x0f0f0f0fu; // x = ---- fedc ---- ba98 ---- 7654 ---- 3210
+        x = (x ^ (x <<  2)) & 0x33333333u; // x = --fe --dc --ba --98 --76 --54 --32 --10
+        x = (x ^ (x <<  1)) & 0x55555555u; // x = -f-e -d-c -b-a -9-8 -7-6 -5-4 -3-2 -1-0
+        return x;
+}
+uint MortonCodeEncode2D(uint x, uint y) {
+        return (Part1By1(y) << 1) + Part1By1(x);
+}
+uvec3 Float3ToUint3(vec3 v) {
+        // Reinterpret value as uint
+        uvec3 value_as_uint = uvec3(floatBitsToUint(v.x), floatBitsToUint(v.y), floatBitsToUint(v.z));
+        // Invert sign bit of positives and whole of  to anegativesllow comparison as unsigned ints
+        value_as_uint.x ^= (1u + ~(value_as_uint.x >> 31u) | 0x80000000u);
+        value_as_uint.y ^= (1u + ~(value_as_uint.y >> 31u) | 0x80000000u);
+        value_as_uint.z ^= (1u + ~(value_as_uint.z >> 31u) | 0x80000000u);
+        return value_as_uint;
+}
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+        vec2 uv = fragCoord/iResolution.xy;
+        //uv.x /= iResolution.y/iResolution.x;
+        // Grid resolution to display
+        const uint res = 1u << 5u;
+        const uint res2 = res * res;
+        const float fres = float(res);
+        // Scaled uv
+        vec3 vuv = vec3(uv * fres, 0.0);
+        // Map floats to unsigned integers
+        #if USE_NEW_MAPPING
+        uvec3 uuv = Float3ToUint3(vuv);
+        #else
+        uvec3 uuv = uvec3(vuv);
+        #endif
+        // Find index on curve of pixel
+        uint curveIndex = MortonCodeEncode2D(uuv.x, uuv.y) % res2;
+        vec3 col = vec3(float(curveIndex) / float(res2));
+        // Color one pixel so we can see the Z-order nature of the curve
+        if (curveIndex == uint(iTime*3.) % res2)
+        col = vec3(1.0, 0.0, 0.0);
+        // Output to screen
+        fragColor = vec4(col,1.0);
+}
+ */
 import Layout from '@theme/Layout'
 import { useGL } from 'glre/src/react'
-import { Fn, Loop, Scope, float, int, iResolution, iTime, uv, vec2, vec3, vec4 } from 'glre/src/node'
+import { Fn, Loop, Scope, float, int, iResolution, iTime, ivec2, uv, vec2, vec3, vec4 } from 'glre/src/node'
 import type { Int, Vec2, Vec4 } from 'glre/src/node'
 
 const MAX_LEVEL = 4
 
-const compact1By1 = Fn(([x]: [Int]): Int => {
-        const v = x.bitAnd(int(0x55555555)).toVar()
-        v.assign(v.bitXor(v.shiftRight(int(1))).bitAnd(int(0x33333333)))
-        v.assign(v.bitXor(v.shiftRight(int(2))).bitAnd(int(0x0f0f0f0f)))
-        v.assign(v.bitXor(v.shiftRight(int(4))).bitAnd(int(0x00ff00ff)))
-        v.assign(v.bitXor(v.shiftRight(int(8))).bitAnd(int(0x0000ffff)))
-        return v
-})
-
-const part1By1 = Fn(([x]: [Int]): Int => {
-        const v = x.bitAnd(int(0x0000ffff)).toVar()
-        v.assign(v.bitXor(v.shiftLeft(int(8))).bitAnd(int(0x00ff00ff)))
-        v.assign(v.bitXor(v.shiftLeft(int(4))).bitAnd(int(0x0f0f0f0f)))
-        v.assign(v.bitXor(v.shiftLeft(int(2))).bitAnd(int(0x33333333)))
-        v.assign(v.bitXor(v.shiftLeft(int(1))).bitAnd(int(0x55555555)))
-        return v
-})
+const mffff = int(0x0000ffff).constant()
+const m00ff = int(0x00ff00ff).constant()
+const m0f0f = int(0x0f0f0f0f).constant()
+const m3333 = int(0x33333333).constant()
+const m5555 = int(0x55555555).constant()
 
 const ij2id = Fn(([ij]: [Vec2]): Int => {
         const p = ij.toIVec2().toVar()
-        return part1By1(p.y).shiftLeft(int(1)).add(part1By1(p.x))
+        p.bitAndAssign(ivec2(mffff))
+        p.bitXorAssign(p.shiftLeft(int(8)))
+        p.bitAndAssign(ivec2(m00ff))
+        p.bitXorAssign(p.shiftLeft(int(4)))
+        p.bitAndAssign(ivec2(m0f0f))
+        p.bitXorAssign(p.shiftLeft(int(2)))
+        p.bitAndAssign(ivec2(m3333))
+        p.bitXorAssign(p.shiftLeft(int(1)))
+        p.bitAndAssign(ivec2(m5555))
+        return p.y.shiftLeft(int(1)).add(p.x)
 })
 
 const id2ij = Fn(([c]: [Int]): Vec2 => {
-        const x = compact1By1(c).toVar()
-        const y = compact1By1(c.shiftRight(int(1))).toVar()
-        return vec2(x.toFloat(), y.toFloat())
+        const p = ivec2(c, c.shiftRight(int(1))).toVar()
+        p.bitAndAssign(ivec2(m5555))
+        p.bitXorAssign(p.shiftRight(int(1)))
+        p.bitAndAssign(ivec2(m3333))
+        p.bitXorAssign(p.shiftRight(int(2)))
+        p.bitAndAssign(ivec2(m0f0f))
+        p.bitXorAssign(p.shiftRight(int(4)))
+        p.bitAndAssign(ivec2(m00ff))
+        p.bitXorAssign(p.shiftRight(int(8)))
+        p.bitAndAssign(ivec2(mffff))
+        return vec2(p)
 })
 
 const segment = Fn(([p, a, b]: [Vec2, Vec2, Vec2]) => {
